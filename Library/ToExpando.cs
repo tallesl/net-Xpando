75d0d0f6bece117e3d518f0fcd27b1552a036a5b
Talles L <talleslasmar@gmail.com>
2015-08-18 14:35:28 -0300

    existing code

---
+++ Library/ToExpando.cs
@@ -0,0 +1,29 @@
+﻿namespace ToExpando
+{
+    using PropertiesHash;
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Linq;
+
+    public static class ToExpandoExtension
+    {
+        /// <summary>
+        /// Makes a ExpandoObject out of the given object.
+        /// </summary>
+        /// <param name="obj">Object to become ExpandoObject</param>
+        /// <returns>The ExpandoObject made</returns>
+        public static ExpandoObject ToExpando(this object obj)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            else if (obj is ExpandoObject) return (ExpandoObject)obj;
+            else
+            {
+                var expando = new ExpandoObject();
+                var dict = (IDictionary<string, object>)expando;
+                foreach (var kvp in PropertiesHasher.Make(obj)) dict.Add(kvp);
+                return expando;
+            }
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,42 @@
+﻿namespace ToExpando.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Null()
+        {
+            ((object)null).ToExpando();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            var expando = new { }.ToExpando();
+            var dict = (IDictionary<string, object>)expando;
+
+            Assert.IsTrue(expando is ExpandoObject);
+            Assert.AreEqual(0, dict.Count);
+        }
+
+        [TestMethod]
+        public void Anonymous()
+        {
+            dynamic expando = new { Foo = "Bar" }.ToExpando();
+            expando.X = "Y";
+
+            var dict = (IDictionary<string, object>)expando;
+
+            Assert.IsTrue(expando is ExpandoObject);
+            Assert.AreEqual(2, dict.Count);
+            Assert.AreEqual("Bar", dict["Foo"]);
+            Assert.AreEqual("Y", dict["X"]);
+        }
+    }
+}
