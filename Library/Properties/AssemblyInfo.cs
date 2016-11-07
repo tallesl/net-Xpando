82822a6453d72b7b7f2d2c09c5427643f3ae27a9
Talles L <talleslasmar@gmail.com>
2016-11-07 16:05:09 -0200

    2.2.0

--- Library/Properties/AssemblyInfo.cs
+++ Library/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Xpando")]
-[assembly: AssemblyDescription("Creates an ExpandoObject out of a given object.")]
+[assembly: AssemblyDescription("Utilities for dealing with ExpandoObject.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
